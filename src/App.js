1abc467bdad7d3cae179b691b6a604a6f8cb8d96
duraidfakhoury <duraid.fakhoury@gmail.com>
2024-09-26 11:33:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import  Container  from 'react-bootstrap/Container';
+import { InputGroup, Row , Col, FormControl, Button, ListGroup} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      userInput : "",
+      list:[]
+    }
+  }
+
+  updateInput(value) { 
+    this.setState({
+      userInput : value
+    })
+  }
+
+  addItem() {
+    if (this.state.userInput !== ""){
+      const userInput = {
+        id : Math.random(),
+        value  : this.state.userInput
+      };
+      const list = [...this.state.list]
+      list.push(userInput);
+      this.setState(
+        {
+          list,
+          userInput:"",
+        }
+      )
+    }
+  }
+ 
+  deleteItem(key){
+    const list = [...this.state.list];
+    const updateList = list.filter((item)=> item.id!== key);
+    this.setState({
+      list:updateList,
+    });
+  }
+
+  editItem = (index) => {
+    const todos = [...this.state.list];
+    const editedTodo = prompt("edit this todo: ");
+    if(editedTodo !==null && editedTodo.trim !== ''){
+      let updatedTodos = [...todos];
+      updatedTodos[index].value=editedTodo;
+      this.setState({
+        list:updatedTodos,
+      })
+    }
+  }
+
+  render() {
+    return(
+     <Container
+     style={{
+     }}
+     >
+      <Row
+        style={{
+          display:'flex',
+          justifyContent:'center',
+          alignContent:'center',
+          fontSize:'3rem',
+          fontWeight:'bolder',
+        }}>TODO LIST</Row>
+      <hr/>
+      <Row>
+        <Col md = {{span : 5 , offset : 4 }}>
+          <InputGroup className='mb-3'>
+            <FormControl
+            placeholder='Add Item ...'
+            size='lg'
+            aria-label='Add somthing'
+            aria-describedby='basic-addon2'
+            value={this.state.userInput}
+            onChange={(item)=>this.updateInput(item.target.value)}
+            />
+            <InputGroup>
+              <Button
+              variant='dark'
+              className='mt-2'
+              onClick={()=>this.addItem()}
+              >Add</Button>
+            </InputGroup>
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{span : 5 , offset : 4 }}>
+          <ListGroup>
+            { this.state.list.map((item,index)=>{
+                return(
+                  <div key = {index}>
+                    <ListGroup.Item
+                      variant='dark'
+                      action 
+                      style={{
+                        display : 'flex',
+                        justifyContent : 'space-between',
+                      }}>
+                        {item.value}
+                        <span>
+                          <Button style={{marginRight:"10px"}} 
+                          variant='light'
+                          onClick={()=>this.deleteItem(item.id)}>
+                            Delete
+                          </Button>
+                          <Button variant='light'
+                          onClick={()=>this.editItem(index)}>
+                            edit
+                          </Button>
+                        </span>
+                      </ListGroup.Item>
+                  </div>
+                )
+              })
+            }
+          </ListGroup>
+        </Col>
+      </Row>
+     </Container> 
+    );
+  }
 }
-
 export default App;
